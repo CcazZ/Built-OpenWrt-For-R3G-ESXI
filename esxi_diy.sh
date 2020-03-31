e01100e3e134f6dfcb2395bd002d7082ca21cc3d
C C <lcc.0705@qq.com>
2020-03-31 16:06:20 +0800

    111

--- esxi_diy.sh
+++ esxi_diy.sh
@@ -6,4 +6,12 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+#删除原主题
+rm -rf package/lean/luci-theme-argon
+#添加新的主题	
+git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
+#取消bootstrap为默认主题	
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#删除默认密码
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
